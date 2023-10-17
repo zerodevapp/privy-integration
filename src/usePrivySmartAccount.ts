f7f962b0ed268d53116bc4aec65b5149a5c0755f
Ankush Swarnakar <ankushswar1@gmail.com>
2023-10-17 14:13:18 -0400

    Update logic for initializing smart wallet, update chainId in usePrivySmartAccount

--- src/usePrivySmartAccount.ts
+++ src/usePrivySmartAccount.ts
@@ -6,15 +6,33 @@ import { useZeroDev } from "./ZeroDevContext"
 export const usePrivySmartAccount = () => {
     const [provider, setProvider] = useState<ECDSAProvider>()
     const [address, setAddress] = useState<string>()
+    const [chainId, setChainId] = useState<string>();
     const { projectId } = useZeroDev()
     const privy = usePrivy()
+    // Check if the user has an embedded wallet
+    const hasEmbeddedWallet = !!privy.user?.linkedAccounts.find((account) => (account.type === 'wallet' && account.walletClientType === 'privy'));
     const { wallets } = useWallets();
+    // Find the embedded wallet's ConnectedWallet object
     const embeddedWallet = useMemo(() => wallets.find((wallet) => (wallet.walletClientType === 'privy')), [wallets]);
 
     useEffect(() => {
+      // Do nothing until Privy is ready
+      if (!privy.ready) return;
+      // If the user is not logged in, clear the provider and address
+      if (!privy.authenticated) {
+        setProvider(undefined);
+        setAddress(undefined);
+        return;
+      }
+
       const initializeZeroDev = async () => {
-        if (!wallets.length) throw new Error('Cannot initialize smart contract wallet without an EOA connected.');
-        // Use embedded wallet if the user has one, otherwise external wallet
+        // If the user has no wallets, do nothig
+        if (!wallets.length) return;
+        // If the user has an embedded wallet, but the embedded wallet object has not yet loaded (may
+        // take some time due to the iframe), do nothing
+        if (hasEmbeddedWallet && !embeddedWallet) return;
+        // Otherwise, initialize a smart wallet using the loaded embedded wallet as a signer,
+        // or the most recently connected external wallet (wallets[0]).
         const provider = await (embeddedWallet || wallets[0]).getEthereumProvider();
         ECDSAProvider.init({
           projectId,
@@ -27,10 +45,12 @@ export const usePrivySmartAccount = () => {
         }).then(async (provider) => {
           setProvider(provider);
           setAddress(await provider.getAddress());
+          const chainIdAsNumber = Number(await provider.request({method: 'eth_chainId'}));
+          setChainId(`eip155:${chainIdAsNumber}`);
         })
       }
-      if (!provider && wallets.length) initializeZeroDev();
-    }, [provider, privy, projectId])
+      initializeZeroDev();
+    }, [privy.ready, privy.authenticated, wallets.length, projectId, provider]);
 
     return useMemo(() => {
       const zeroDevReady = !!provider && !!address
@@ -38,7 +58,7 @@ export const usePrivySmartAccount = () => {
         ...privy,
         user: {
           ...privy.user,
-          wallet: privy.user?.wallet ? { ...privy.user.wallet, address } : undefined
+          wallet: privy.user?.wallet ? { ...privy.user.wallet, address, chainId } : undefined
         },
         zeroDevReady,
         sendTransaction: (...args: Parameters<typeof privy.sendTransaction>) => {
